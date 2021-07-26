21bffdc6871e463de2b72b8574bda147dbb79798
Jun Kurihara <kurihara@ieee.org>
2021-07-26 22:03:36 +0900

    add docker

---
+++ docker/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+echo "start token server"
+
+# read custom configuration
+source /opt/token-server/etc/.env
+
+# echo "doh-proxy: upstream dns server address: ${UPSTREAM_ADDR}:${UPSTREAM_PORT}"
+
+/opt/token-server/sbin/rust-token-server init \
+  --admin-name ${ADMIN_NAME} \
+  --admin-password ${ADMIN_PASSWORD} \
+  --db-file-path /opt/token-server/etc/userdb.db
+
+/opt/token-server/sbin/rust-token-server run \
+  --signing-algorithm ${SIGNING_ALGORITHM} \
+  --signing-key-path /opt/token-server/etc/private_key.pem \
+  --db-file-path /opt/token-server/etc/userdb.db
