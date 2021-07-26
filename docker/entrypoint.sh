bede4e3f9d34f03c65bef81df83922f4ba2e99cb
Jun Kurihara <kurihara@ieee.org>
2021-07-26 22:53:23 +0900

    fix entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -7,12 +7,17 @@ source /opt/token-server/etc/.env
 
 # echo "doh-proxy: upstream dns server address: ${UPSTREAM_ADDR}:${UPSTREAM_PORT}"
 
+echo "init first"
 /opt/token-server/sbin/rust-token-server init \
   --admin-name ${ADMIN_NAME} \
   --admin-password ${ADMIN_PASSWORD} \
   --db-file-path /opt/token-server/etc/userdb.db
 
-ROCKET_ENV=production /opt/token-server/sbin/rust-token-server run \
+echo "run the server"
+ROCKET_ENV=production \
+ROCKET_ADDRESS=0.0.0.0 \
+ROCKET_PORT=8000 \
+/opt/token-server/sbin/rust-token-server run \
   --signing-algorithm ${SIGNING_ALGORITHM} \
   --signing-key-path /opt/token-server/etc/private_key.pem \
   --db-file-path /opt/token-server/etc/userdb.db
