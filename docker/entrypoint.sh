506d5ff9f57b0d86128a38e4d9e5e12d529dcae9
Jun Kurihara <kurihara@ieee.org>
2021-07-27 00:53:03 +0900

    update entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -9,7 +9,7 @@ echo "init first"
 RUST_LOG=debug /opt/token-server/sbin/rust-token-server init \
   --admin-name ${ADMIN_NAME} \
   --admin-password ${ADMIN_PASSWORD} \
-  --db-file-path /opt/token-server/etc/userdb.db
+  --db-file-path /opt/token-server/var/userdb.db
 
 echo "run the server"
 RUST_LOG=debug \
@@ -19,4 +19,4 @@ ROCKET_PORT=8000 \
 /opt/token-server/sbin/rust-token-server run \
   --signing-algorithm ${SIGNING_ALGORITHM} \
   --signing-key-path /opt/token-server/etc/private_key.pem \
-  --db-file-path /opt/token-server/etc/userdb.db
+  --db-file-path /opt/token-server/var/userdb.db
