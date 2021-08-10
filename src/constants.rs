a93d0dc3ee02db23d6bef9ae9ee52c332e99e00f
Jun Kurihara <kurihara@ieee.org>
2021-08-11 02:15:23 +0900

    fix Dockerfile

--- src/constants.rs
+++ src/constants.rs
@@ -21,4 +21,4 @@ pub const SUBSCRIBER_ID_LEN: usize = 32;
 
 pub const JWT_DURATION_MINS: usize = 30; // TODO: Clapで設定できるように
 pub const REFRESH_TOKEN_LEN: usize = 64;
-pub const REFRESH_TOKEN_DURATION_MINS: usize = 30 * 24 * 60; // 30days
+pub const REFRESH_TOKEN_DURATION_MINS: usize = 30 * 24 * 60; // TODO: 30days, clapで設定できるように
